cf8ff2bbf3c67913e80286965b18ad980c221bd3
mubarok-hub <ridjzal24@gmail.com>
2025-07-01 07:48:58 -0300

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+simplyCountdown('.simply-countdown', {
+  year: 2026,
+  month: 1,
+  day: 8,
+  hours: 8,
+  words: {
+    days: { singular: 'hari', plural: 'hari' },
+    hours: { singular: 'jam', plural: 'jam' },
+    minutes: { singular: 'menit', plural: 'menit' },
+    seconds: { singular: 'detik', plural: 'detik' }
+  },
+});
